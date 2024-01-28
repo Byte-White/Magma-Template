33b964912e1a1738ac6c7e4226bd58d2fa611095
Byte-Wh1te <bytewh1te@yahoo.com>
2024-01-28 17:48:24 +0200

    initial commit + simple template

---
+++ app/src/main.cpp
@@ -0,0 +1,30 @@
+#include "Magma/Magma.h"
+
+class MyApplication:public mg::Application
+{
+public:
+	MyApplication()
+	{
+		window_width = 800;
+		window_height = 600;
+		window_title = "Magma Application";
+	}
+	~MyApplication()
+	{
+
+	}
+	void Render() override
+	{
+		ImGui::Begin("Magma App Template");
+    if(mg::renderer->GetAPI() == mg::RenderAPI::Vulkan)
+      ImGui::Text("Render API: Vulkan");
+    else
+      ImGui::Text("Render API: OpenGL");
+		ImGui::End();
+	}
+};
+
+mg::Application* CreateApplication(int argc, char** argv)
+{
+	return new MyApplication();
+}
